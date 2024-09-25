51ef92e32f5d2743523828d98cf3554133f6ebe1
Oluwatamilore Oladejo <OPOladejo21@my.fisk.edu>
2024-09-25 11:36:04 -0500

    Initial Commit

---
+++ ConnectX/ConnectXApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectXApp.swift
+//  ConnectX
+//
+//  Created by Tamilore Oladejo on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct ConnectXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
