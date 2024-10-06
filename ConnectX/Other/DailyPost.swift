52840793dd925276e0a111e77076dc0e64592ba7
Oluwatamilore Oladejo <OPOladejo21@my.fisk.edu>
2024-10-05 19:10:19 -0500

    Completed Feed View
    - Completed FeedView Design
    - Created sample data
    - Connected FeedView with PostView

---
+++ ConnectX/Other/DailyPost.swift
@@ -0,0 +1,15 @@
+//
+//  SamplePosts.swift
+//  ConnectX
+//
+//  Created by Tamilore Oladejo on 10/5/24.
+//
+
+import Foundation
+
+struct Post {
+    var title: String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var theme: Theme
+}
