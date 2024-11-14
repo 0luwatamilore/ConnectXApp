520853ec286054fc9c1df46612ad39d4671c8fb1
Oluwatamilore Oladejo <OPOladejo21@my.fisk.edu>
2024-11-13 21:08:30 -0600

    FeedView - added post feature where user can post a message and it stores it in database

---
+++ ConnectX/Other/Post.swift
@@ -0,0 +1,9 @@
+//
+//  Post.swift
+//  ConnectX
+//
+//  Created by Tamilore Oladejo on 11/10/24.
+//
+
+import Foundation
+
---
+++ ConnectX/Views/CreatePostView.swift
@@ -0,0 +1,18 @@
+//
+//  CreatePostView.swift
+//  ConnectX
+//
+//  Created by Tamilore Oladejo on 11/10/24.
+//
+
+import SwiftUI
+
+struct CreatePostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreatePostView()
+}
